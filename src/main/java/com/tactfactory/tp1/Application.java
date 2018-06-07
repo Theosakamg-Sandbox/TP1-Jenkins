7dca0f3aeca3ac97247a739459d259fac6826d1a
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2018-06-07 12:23:26 +0200

    Final fix PMD.

--- src/main/java/com/tactfactory/tp1/Application.java
+++ src/main/java/com/tactfactory/tp1/Application.java
@@ -1,17 +1,5 @@
 /*******************************************************************************
  * Copyright 2018 Mickael Gaillard <mickael.gaillard@tactfactory.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License.  You may obtain a copy
- * of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations under
- * the License.
  ******************************************************************************/
 
 package com.tactfactory.tp1;
@@ -25,13 +13,12 @@ import org.joda.time.DateTime;
  */
 public final class Application {
 
+    /** State of application. (true if running). */
+    private static boolean isRunning;
+
     private Application() {
         super();
     }
-
-    /** State of application. (true if running). */
-    private static boolean isRunning = false;
-
 
     /**
      * Entry-point of application.
@@ -51,10 +38,12 @@ public final class Application {
      * @return result of addition.
      */
     public static int add(final int value1, final int value2) {
-        int result = Integer.MIN_VALUE;
+        int result;
 
         if (isRunning) {
             result = value1 + value2;
+        } else {
+            result = Integer.MIN_VALUE;
         }
 
         return result;
